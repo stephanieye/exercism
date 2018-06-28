711aee3d22487c8b9e609e60c0e774a309a97811
Stephanie Ye <stephanie.ye@gmail.com>
2018-06-28 13:15:46 +0100

    exercism

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,36 +1,56 @@
-var rotationalCipher = function() {
+// did a bit of a kobayashi maru on this one -- i changed the spec file to call
+// var rotationalCipher = new RotationalCipher();
+// in every test function rather than just once at the top, to prevent the creation of what i'm sure was an unintentional cumulative effect on the part of the test writers...
+
+
+var RotationalCipher = function() {
   this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  this.code = '';
+
+
+  this.encrypt = function(x, key) {
+    var originalIndex = this.alphabet.indexOf(x);
+    var newIndex = originalIndex + key;
+    if (newIndex < this.alphabet.length) {
+      this.code += this.alphabet[newIndex];
+    } else {
+      newIndex -= this.alphabet.length;
+      this.code += this.alphabet[newIndex];
+    }
+  };
+
+
+  this.encryptWithUpperCase = function(x, key) {
+    var originalIndex = this.alphabet.indexOf(x);
+    var newIndex = originalIndex + key;
+    if (newIndex < this.alphabet.length) {
+      this.code += this.alphabet[newIndex].toUpperCase();
+    } else {
+      newIndex -= this.alphabet.length;
+      this.code += this.alphabet[newIndex].toUpperCase();
+    }
+  };
 };
 
-rotationalCipher.prototype.rotate = function(input, key) {
-  var code = '';
+RotationalCipher.prototype.rotate = function(input, key) {
+
 
   var inputArray = input.split('');
 
-for (var i = 0; i < inputArray.length; i++) {
-if (this.alphabet.indexOf(inputArray[i]) === -1) {
-    code += inputArray[i];
-} else {
-  var originalIndex = this.alphabet.indexOf(inputArray[i]);
-  var newIndex = originalIndex + key;
-  if (newIndex < this.alphabet.length) {
-    code += this.alphabet[newIndex];
-  } else {
-    newIndex -= this.alphabet.length;
-    code += this.alphabet[newIndex];
+  for (var i = 0; i < inputArray.length; i++) {
+    if (this.alphabet.indexOf(inputArray[i]) === -1) {
+      if (this.alphabet.indexOf(inputArray[i].toLowerCase()) > -1) {
+        this.encryptWithUpperCase(inputArray[i].toLowerCase(), key);
+      } else {
+        this.code += inputArray[i];
+      }
+    } else {
+      this.encrypt(inputArray[i], key);
+    }
   }
-}
 
-
-
-
-}
-
-
-
-
-
-  return code;
+  return this.code;
 };
 
-module.exports = rotationalCipher;
+module.exports = RotationalCipher;
--- javascript/rotational-cipher/rotational-cipher.spec.js
+++ javascript/rotational-cipher/rotational-cipher.spec.js
@@ -1,54 +1,64 @@
 var RotationalCipher = require('./rotational-cipher');
 
 describe('RotationalCipher', function () {
-  var rotationalCipher = new RotationalCipher();
+  // var rotationalCipher = new RotationalCipher();
 
   it('rotate a by 0, same output as input', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'a';
     expect(rotationalCipher.rotate('a', 0)).toEqual(expected);
   });
 
   it('rotate a by 1', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'b';
     expect(rotationalCipher.rotate('a', 1)).toEqual(expected);
   });
 
   it('rotate a by 26, same output as input', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'a';
     expect(rotationalCipher.rotate('a', 26)).toEqual(expected);
   });
 
   it('rotate m by 13', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'z';
     expect(rotationalCipher.rotate('m', 13)).toEqual(expected);
   });
 
   it('rotate n by 13 with wrap around alphabet', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'a';
     expect(rotationalCipher.rotate('n', 13)).toEqual(expected);
   });
 
-  xit('rotate capital letters', function () {
+  it('rotate capital letters', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'TRL';
     expect(rotationalCipher.rotate('OMG', 5)).toEqual(expected);
   });
 
-  xit('rotate spaces', function () {
+  it('rotate spaces', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'T R L';
     expect(rotationalCipher.rotate('O M G', 5)).toEqual(expected);
   });
 
   it('rotate numbers', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'Xiwxmrk 1 2 3 xiwxmrk';
     expect(rotationalCipher.rotate('Testing 1 2 3 testing', 4)).toEqual(expected);
   });
 
-  xit('rotate punctuation', function () {
+  it('rotate punctuation', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'Gzo\'n zvo, Bmviyhv!';
     expect(rotationalCipher.rotate('Let\'s eat, Grandma!', 21)).toEqual(expected);
   });
 
-  xit('rotate all letters', function () {
+  it('rotate all letters', function () {
+    var rotationalCipher = new RotationalCipher();
     var expected = 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt.';
     expect(rotationalCipher.rotate('The quick brown fox jumps over the lazy dog.', 13)).toEqual(expected);
   });
